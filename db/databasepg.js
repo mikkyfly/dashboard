df7736458a190fc211a2c04bdd031babf49638d5
Michael <65642892+mikkyfly@users.noreply.github.com>
2023-12-09 22:57:24 +0500

    add func dbpgsql

--- db/databasepg.js
+++ db/databasepg.js
@@ -1,50 +1,37 @@
-
-
 const {Client} = require('pg');
 
 const client = new Client({
-    host:"192.168.0.122",
-    user: "postgres",
-    port: 5432,
-    password:"m5nzk3i2",
-    database: "snmp"
+  host: "localhost",
+  user: "postgres",
+  port: 5432,
+  password: "QWE123!",
+  database: "test_bd"
 });
-
 
 client.connect();
 
-while(true){
-    setTimeout(function(){
-        client.query(`Select * from ups`, (err, res)=>{
-            if(!err){
-                console.log(res.rows);
-            }else{
-                console.log(err.message);
-            }
-            client.end;
-        });
-    },1000);
-}
-/*for (let i = 0; i < 5; i++) {
-    setTimeout(function(){
-        client.query(`Select * from ups`, (err, res)=>{
-            if(!err){
-                console.log(res.rows);
-            }else{
-                console.log(err.message);
-            }
-            client.end;
-        });
-    },1000);
-    
-}
-
-
-client.query(`Select * from ups`, (err, res)=>{
-    if(!err){
-        console.log(res.rows);
-    }else{
-        console.log(err.message);
+setInterval(function(){
+  client.query(`SELECT value_state FROM test ORDER BY value_state DESC LIMIT 1`,(err,res)=>{
+    if (!err) {
+      console.log(res.rows);
+    }
+    else{
+      console.log(err.message);
     }
     client.end;
-});*/
+  })
+}
+,1000);
+
+setInterval(function(){
+  client.query(`SELECT value_state FROM test_2 ORDER BY value_state DESC LIMIT 1`,(err,res)=>{
+    if (!err) {
+      console.log(res.rows);
+    }
+    else{
+      console.log(err.message);
+    }
+    client.end;
+  })
+}
+,1000);
